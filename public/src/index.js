03bb20a8463b91ccaa2e43104cc2f4106cad1f63
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2024-01-09 11:27:22 +0530

    chore: cleanup comments
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- public/src/index.js
+++ public/src/index.js
@@ -66,11 +66,6 @@ const main = async () => {
     ];
     const wasi = new WASI(args, env, fds);
 
-    // const wasm = await WebAssembly.compileStreaming(fetch("bin.wasm"));
-    // let inst = await WebAssembly.instantiate(wasm, {
-    //     "wasi_snapshot_preview1": wasi.wasiImport,
-    // });
-    // let WASM_MEMORY = null;
     let WASM_INSTANCE = null;
     const load_string = (ptr, len) => {
         if (!WASM_INSTANCE) throw new Error('the wasm instance is missing');
@@ -81,84 +76,27 @@ const main = async () => {
         // console.log('load_wasm_module called with:', s);
         return s;
     };
-    // const load_wasm_module = (wasmModulePathPtr, wasmModulePathLength) => {
-    //     // console.log('load_wasm_module wasmModulePathPtr', wasmModulePathPtr, 'wasmModulePathLength', wasmModulePathLength);
-    //     // if (!WASM_MEMORY) throw new Error('the wasm memory is missing');
-    //     if (!WASM_INSTANCE) throw new Error('the wasm instance is missing');
-    //     // console.log('WASM_MEMORY', WASM_MEMORY, WASM_MEMORY.buffer.detached);
-    //     // console.log('load_wasm_module WASM_INSTANCE', typeof WASM_INSTANCE, WASM_INSTANCE);
-    //     const memory = new Uint8Array(WASM_INSTANCE.exports.memory.buffer);
-    //     // console.log('load_wasm_module memory', memory, memory.length, memory.byteLength);
-    //     const buf = memory.slice(wasmModulePathPtr, wasmModulePathPtr + wasmModulePathLength);
-    //     const decoder = new TextDecoder('utf-8');
-    //     const s = decoder.decode(buf);
-    //     // console.log('load_wasm_module buf:', buf, 's:', s);
-    //     console.log('load_wasm_module called with:', s);
-    //     return 0;
-    // };
     const load_wasm_module = (wasmModulePathPtr, wasmModulePathLength) => {
         const s = load_string(wasmModulePathPtr, wasmModulePathLength);
         console.log('load_wasm_module called with:', s);
-        // const myModule = new WebAssembly.Module(wasmBytes);
+        // https://developer.mozilla.org/en-US/docs/WebAssembly/JavaScript_interface/Module/Module
         const myModule = new WebAssembly.Module(fibWasmBytes);
         const importObject = {
             'console': {
                 'log': console.log
             }
         };
+        // https://developer.mozilla.org/en-US/docs/WebAssembly/JavaScript_interface/Instance/Instance
         const instance = new WebAssembly.Instance(myModule, importObject);
         console.log('compiled wasm and made an instance:', instance);
         return 42;
     };
-    // TODO: preload modules because compile and instantiate are async operations
-    // This also means that we have to unzip the archive in javascript
-    // TODO: or use synchronous compilation new WebAssembly.Module(bufferSource)
-    // https://developer.mozilla.org/en-US/docs/WebAssembly/JavaScript_interface/Module/Module
-    // Warning: Since compilation for large modules can be expensive, developers
-    // should only use the Module() constructor when synchronous compilation is
-    // absolutely required; the asynchronous WebAssembly.compileStreaming()
-    // method should be used at all other times.
-    // TODO: and use sync instantiate new WebAssembly.Instance(module, importObject);
-    // https://developer.mozilla.org/en-US/docs/WebAssembly/JavaScript_interface/Instance/Instance
-    // Warning: Since instantiation for large modules can be expensive, developers should only use
-    // the Instance() constructor when synchronous instantiation is absolutely required; the asynchronous
-    // WebAssembly.instantiateStreaming() method should be used at all other times.
     const run_transform = (wasmModuleId, inputJsonPtr, inputJsonLength) => {
         const s = load_string(inputJsonPtr, inputJsonLength);
         const input = JSON.parse(s);
         console.log('run_transform called with: wasmModuleId:', wasmModuleId, 's:', s, 'input:', input);
         return 0;
     };
-    // const orig_fn = wasi.wasiImport['fd_write'];
-    // wasi.wasiImport['fd_write'] = (...args) => {
-    //     console.log('fd_write args:', ...args);
-    //     console.log('wasi.inst', wasi.inst === WASM_INSTANCE);
-    //     const arrBuf = WASM_INSTANCE.exports.memory.buffer;
-    //     console.log('arrBuf', arrBuf);
-    //     const mem = new Uint8Array(arrBuf);
-    //     console.log('mem', mem);
-    //     const view = new DataView(arrBuf);
-    //     const fd = args[0];
-    //     const iovs_ptr = args[1];
-    //     const iovs_len = args[2];
-    //     const nwritten_ptr = args[3];
-    //     console.log('fd', fd, 'iovs_ptr', iovs_ptr, 'iovs_len', iovs_len, 'nwritten_ptr', nwritten_ptr);
-    //     const iovecs = ciovec_read_bytes_array(
-    //         view,
-    //         iovs_ptr,
-    //         iovs_len,
-    //     );
-    //     console.log('iovecs', iovecs);
-    //     for (let iovec of iovecs) {
-    //         console.log('reading iovec', iovec);
-    //         const b = mem.slice(iovec.buf, iovec.buf + iovec.buf_len);
-    //         const decoder = new TextDecoder();
-    //         const s = decoder.decode(b);
-    //         console.log('reading iovec buffer', b, 's', s);
-    //     }
-    //     const result = orig_fn(...args);
-    //     return result;
-    // };
     const importObject = {
         "wasi_snapshot_preview1": wasi.wasiImport,
         'mym2kmodule': {
@@ -169,13 +107,9 @@ const main = async () => {
     const wasmModuleInstance = await WebAssembly.instantiate(wasmModule, importObject);
     console.log('wasmModuleInstance', typeof wasmModuleInstance, wasmModuleInstance);
     WASM_INSTANCE = wasmModuleInstance;
-    // const memory = new Uint8Array(wasmModuleInstance.exports.memory.buffer);
-    // console.log('memory', memory);
-    // WASM_MEMORY = memory;
 
     console.log('starting wasm');
     wasi.start(wasmModuleInstance);
-    // console.log('wasi.inst', wasi.inst, wasi.inst === WASM_INSTANCE);
 
     console.log('main done');
 };
